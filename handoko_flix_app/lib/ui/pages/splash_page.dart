bb50d22cb55a7503079d9dc60ef5d306bdb6d61f
Siswo Handoko <cs.siswo.handoko@gmail.com>
2020-07-19 21:31:58 +0700

    create splash screen and add assets and setup global style

--- handoko_flix_app/lib/ui/pages/splash_page.dart
+++ handoko_flix_app/lib/ui/pages/splash_page.dart
@@ -4,13 +4,67 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Splash Page")),
-        body: Center(
-            child: RaisedButton(
-          child: Text("Go To Sign In Page"),
-          onPressed: () {
-            context.bloc<PageBloc>().add(GoToLoginPage());
-          },
-        )));
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            //LOGO FLUTIX
+            Container(
+              height: 136,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("assets/logo.png"))),
+            ),
+            //NEW EXPERIENCE TEXT
+            Container(
+              margin: EdgeInsets.only(top: 70, bottom: 16),
+              child: Text(
+                "Pengalaman Baru",
+                style: blackTextFont.copyWith(fontSize: 20),
+              ),
+            ),
+            Text(
+              "Tonton Film Terbaru Dengan Pengalaman\n Yang Lebih Baik Dari Biasanya",
+              style: greyTextFont.copyWith(
+                  fontSize: 16, fontWeight: FontWeight.w300),
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              width: 250,
+              height: 46,
+              margin: EdgeInsets.only(top: 70, bottom: 19),
+              child: RaisedButton(
+                child: Text(
+                  "Mulai",
+                  style: whiteTextFont.copyWith(fontSize: 16),
+                ),
+                color: mainColor,
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Anda Telah Memiliki Akun? ",
+                  style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    context.bloc<PageBloc>().add(GoToLoginPage());
+                  },
+                  child: Text(
+                    "Masuk",
+                    style: purpleTextFont,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
